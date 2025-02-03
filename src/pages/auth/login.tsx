ae4aca31337467d550e6268704878d7cffc3f5b8
Jesse <Jessedekoe03@gmail.com>
2025-02-03 22:25:34 +0100

    Various changes

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,40 +1,68 @@
 import '../../../public/css/login.css'
+import '../../../public/css/components.css'
 import {Col, Row} from "react-bootstrap";
+import useIsMobileView from "../../hooks/isMobile.ts";
+
+const images = [
+    {src: "src/assets/images/scenic-background-light.jpg"},
+    {src: "src/assets/images/scenic-background-dark.jpg"}
+];
 
 function Login() {
-    const scenicImage = "src/assets/images/mountains-7292778_1280.jpg";
 
+    const imageUrl = images[Math.floor(Math.random() * images.length)].src;
+
+    const isMobile = useIsMobileView();
     return (
         <>
-            <div className="backgroundImage d-flex justify-content-center align-items-center" style={{backgroundImage: `url(${scenicImage})` }}>
-                <div className="p-5 loginContainer innerContainer">
-                    <Row>
+            {/*Scenic Image*/}
+            <div className="backgroundImage d-flex justify-content-center align-items-center"
+                 style={{backgroundImage: `url(${imageUrl})`}}>
+
+                {/*Login Container*/}
+                <div className={`${isMobile ? "py-5 px-3 w-75" : "p-5 w-50"} loginContainer`}>
+                    <Row className={"pb-1 border-bottom mb-3"}>
                         <Col xs={12} sm={12} md={12} lg={12} xl={12}>
                             <h2>Login</h2>
                         </Col>
                     </Row>
                     <Row>
-                        <Col xs={6} sm={6} md={6} lg={6} xl={6}>
+                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className={"d-flex align-items-center"}>
                             <label htmlFor="email">Email: </label>
                         </Col>
-                        <Col xs={6} sm={6} md={6} lg={6} xl={6}>
-                            <input id={"email"} type="email" placeholder="name@example.com" />
+                        <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                            <input className={"w-100 px-1"} id={"email"}
+                                   style={{
+                                       borderRadius: `4px`,
+                                       borderWidth: `1px`,
+                                       borderStyle: `solid`,
+                                       borderColor: `var(--mc-gray)`
+                                   }} type="email"
+                                   placeholder="name@example.com"/>
                         </Col>
                     </Row>
                     <Row className={"py-2"}>
-                        <Col xs={6} sm={6} md={6} lg={6} xl={6}>
+                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className={"d-flex align-items-center"}>
                             <label htmlFor="password">Password: </label>
                         </Col>
-                        <Col xs={6} sm={6} md={6} lg={6} xl={6}>
-                            <input id={"password"} type="password" />
+                        <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                            <input className={"w-100 px-1"} id={"password"} style={{
+                                borderRadius: `4px`,
+                                borderWidth: `1px`,
+                                borderStyle: `solid`,
+                                borderColor: `var(--mc-gray)`
+                            }} type="password"/>
                         </Col>
                     </Row>
-                    <Row className={"py-2 flex-row-reverse"}>
-                        <Col xs={6} sm={6} md={6} lg={6} xl={6}>
-
+                    <Row className={"py-2"}>
+                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className={"d-flex align-items-center"}>
+                            <small className={"fst-italic text-muted"}>
+                                Trouble signing in? <a href="" className={"routingAnchor"}>click here.</a>
+                            </small>
                         </Col>
-                        <Col xs={6} sm={6} md={6} lg={6} xl={6}>
-                            <button type="submit"> login </button>
+                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className={"d-flex justify-content-end align-items-center"}>
+                            <button className={"px-3 py-1 submitButton"} type="submit"> login
+                            </button>
                         </Col>
                     </Row>
                 </div>
