e324a702db1baad3f584586142ddc33fb87f12c2
Jesse de Koe <jdekoe@logictrade.nl>
2025-02-04 09:11:22 +0100

    Added some classes and made it very responsive

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -20,48 +20,37 @@ function Login() {
                  style={{backgroundImage: `url(${imageUrl})`}}>
 
                 {/*Login Container*/}
-                <div className={`${isMobile ? "py-5 px-3 w-75" : "p-5 w-50"} loginContainer`}>
+                <div className={`${isMobile ? "pt-5 pb-3 px-3 w-75" : "p-5 w-50"} loginContainer`}>
                     <Row className={"pb-1 border-bottom mb-3"}>
                         <Col xs={12} sm={12} md={12} lg={12} xl={12}>
                             <h2>Login</h2>
                         </Col>
                     </Row>
                     <Row>
-                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className={"d-flex align-items-center"}>
+                        <Col xs={12} sm={12} md={4} lg={6} xl={6} className={"d-flex align-items-center"}>
                             <label htmlFor="email">Email: </label>
                         </Col>
-                        <Col xs={12} sm={12} md={6} lg={6} xl={6}>
-                            <input className={"w-100 px-1"} id={"email"}
-                                   style={{
-                                       borderRadius: `4px`,
-                                       borderWidth: `1px`,
-                                       borderStyle: `solid`,
-                                       borderColor: `var(--mc-gray)`
-                                   }} type="email"
+                        <Col xs={12} sm={12} md={8} lg={6} xl={6}>
+                            <input className={"w-100 px-1 inputField"} id={"email"} type="email"
                                    placeholder="name@example.com"/>
                         </Col>
                     </Row>
                     <Row className={"py-2"}>
-                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className={"d-flex align-items-center"}>
+                        <Col xs={12} sm={12} md={4} lg={6} xl={6} className={"d-flex align-items-center"}>
                             <label htmlFor="password">Password: </label>
                         </Col>
-                        <Col xs={12} sm={12} md={6} lg={6} xl={6}>
-                            <input className={"w-100 px-1"} id={"password"} style={{
-                                borderRadius: `4px`,
-                                borderWidth: `1px`,
-                                borderStyle: `solid`,
-                                borderColor: `var(--mc-gray)`
-                            }} type="password"/>
+                        <Col xs={12} sm={12} md={8} lg={6} xl={6}>
+                            <input className={"w-100 px-1 inputField"} id={"password"} type="password"/>
                         </Col>
                     </Row>
                     <Row className={"py-2"}>
-                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className={"d-flex align-items-center"}>
-                            <small className={"fst-italic text-muted"}>
+                        <Col xs={12} sm={8} md={6} lg={6} xl={6} className={"d-flex align-items-center"}>
+                            <small className={"fst-italic text-muted text-nowrap"}>
                                 Trouble signing in? <a href="" className={"routingAnchor"}>click here.</a>
                             </small>
                         </Col>
-                        <Col xs={12} sm={12} md={6} lg={6} xl={6} className={"d-flex justify-content-end align-items-center"}>
-                            <button className={"px-3 py-1 submitButton"} type="submit"> login
+                        <Col xs={12} sm={4} md={6} lg={6} xl={6} className={`d-flex ${isMobile ? "justify-content-start pt-3" : "justify-content-end"} align-items-center`}>
+                            <button className={`${isMobile ? "w-100" : "px-3"} py-1 submitButton`} type="submit"> login
                             </button>
                         </Col>
                     </Row>
