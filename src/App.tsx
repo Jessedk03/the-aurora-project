e42c079593471216761e8a9b304f0767782a5ca1
Jesse de Koe <jdekoe@logictrade.nl>
2025-02-04 17:01:47 +0100

    Route Paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import {Col, Container, Row} from "react-bootstrap";
 import '../public/css/index.css';
+import {Route, Routes} from "react-router";
+import Login from "./pages/auth/login.tsx";
 
 function App() {
-
-  return (
-    <>
-      <Row className={"w-100 h-100"}>
-          {/*Header*/}
-          <Row className={"border-bottom"}>
-              <Col md={1} className={"ps-4 border-end"}> hallo</Col>
-              <Col md={11}> hier komt een hoop tekst voor de header</Col>
-          </Row>
-          {/*Container*/}
-          <Row className={"h-100"}>
-              <Col md={1} className={"ps-4 border-end"}>ja ja</Col>
-              <Col md={11} className={"backgroundColor"}>
-                  <Container className={"h-100 py-2"}>
-                      hier moet dan content komen
-                  </Container>
-              </Col>
-          </Row>
-      </Row>
-    </>
-  )
+    return (
+        <Routes>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/" element={<Login/>}/>
+        </Routes>
+    );
 }
 
 export default App
