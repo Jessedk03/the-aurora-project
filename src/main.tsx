123892b8fa0b48c58e6b303d15fbe64c3579db69
Jesse de Koe <jdekoe@logictrade.nl>
2025-02-04 17:03:39 +0100

    React Router package added

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../public/css/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from "./pages/auth/login.tsx";
-// import App from "./App.tsx";
+import {BrowserRouter} from "react-router";
+import App from "./App.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Login />
-  </StrictMode>,
+    <BrowserRouter>
+        <StrictMode>
+            <App />
+        </StrictMode>
+    </BrowserRouter>,
 )
